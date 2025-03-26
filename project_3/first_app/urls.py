0eb919ca1f41e26d5313605acbf2c8e606a039ef
Ishrakul-Tahmid <ishrakultahmid@gmail.com>
2025-03-26 23:05:39 +0600

    Module 14.5 updated

--- project_3/first_app/urls.py
+++ project_3/first_app/urls.py
@@ -1,7 +1,6 @@
 
-from django.urls import path, include
+from django.urls import path
 from . import views
 urlpatterns = [
-    # path('first_app/', include('first_app.urls')),
     path('', views.home)
 ]
